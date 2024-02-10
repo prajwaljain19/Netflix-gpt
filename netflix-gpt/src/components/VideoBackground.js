19216b2e5c019d04c080592da6b88d66ec015816
prajwaljain19 <prajwalbopalkar24@gmail.com>
2024-02-10 20:09:23 +0530

    first commit

--- netflix-gpt/src/components/VideoBackground.js
+++ netflix-gpt/src/components/VideoBackground.js
@@ -26,13 +26,15 @@ const VideoBackground = ({ movieId }) => {
   useEffect(() => {
     getMovieVideo();
   
-  }, [movieId]);
+  }, []);
 
   return (
     <div className="w-screen">
       <iframe 
       className="w-screen aspect-video"
-        src={"https://www.youtube.com/embed/" + trailerId}
+        src={
+          "https://www.youtube.com/embed/" + trailerId + "?&autoplay=1&mute=1"
+        }
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       ></iframe>
